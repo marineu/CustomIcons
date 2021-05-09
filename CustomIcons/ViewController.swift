08e17d0c7d63b39cfaa0985174243a6d35a105b5
Marin Ipati <>
2021-05-09 12:53:23 +0100

    Initial project source files

---
+++ CustomIcons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomIcons
+//
+//  Created by Marin Ipati on 09/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
